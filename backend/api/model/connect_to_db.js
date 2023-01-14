814054888244c573c6b1549dc91575a0f29e793e
Thierno DIALLO <thiernomb.diallo@gmail.com>
2023-01-15 00:55:15 +0100

    resume work in asus laptop

--- backend/api/model/connect_to_db.js
+++ backend/api/model/connect_to_db.js
@@ -2,7 +2,15 @@ require("dotenv/config");
 let mongoose = require("mongoose");
 
 const connectToDB = () => {
-    mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    // mongoose.connect('mongodb://'+process.env.MONGODB_USERNAME+':'+process.env.MONGODB_PASSWORD+'@127.0.0.1:27017/samwaktou', {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+    mongoose.connect('mongodb://127.0.0.1:27017/samwaktou', 
+    {
+        authSource: admin,
+        user: "root",
+        pass: "mypass",
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    }, (err) => {
         if(err) return Promise.reject(err);
     });
 }
